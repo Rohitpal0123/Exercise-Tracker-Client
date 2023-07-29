0e5c4924cb2a9f7923ee92a97f2fdc83ca416179
Rohit Pal <rohit.coding.0123@gmail.com>
2023-07-29 20:23:17 +0530

    Completed the backend code

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/user.model");
+let User = require("../models/user.model");
 
 router.route("/").get((req, res) => {
   User.find()
@@ -10,7 +10,7 @@ router.route("/").get((req, res) => {
 router.route("/add").post((req, res) => {
   const username = req.body.username;
 
-  const newUser = new User(username);
+  const newUser = new User({ username });
 
   newUser
     .save()
